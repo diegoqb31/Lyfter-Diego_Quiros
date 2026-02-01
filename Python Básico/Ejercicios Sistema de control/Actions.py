5846501eb456b4b9e7e2686ec8272c7a4ae6bed3
Diego Quiros <quirosbdiego@gmail.com>
2026-02-01 14:16:44 -0600

    Separate Main logic and validate grades above 100

--- Python Básico/Ejercicios Sistema de control/Actions.py
+++ Python Básico/Ejercicios Sistema de control/Actions.py
@@ -1,89 +1,6 @@
 import Menu
 import Data
 
-def start_menu( ):
-    """
-    Inicia el programa, llama a la funcion que muestra el menú y a la que pide opción al usuario,
-    guía la ejecución del programa, a través de sus 7 opciones principales.
-    Función diseñada para ser llamada desde el main.
-
-    Parámetros:
-        No recibe parámetros.
-
-    Retorna:
-        La función no retorna nada.
-    """
-    students_list = []
-    option = 0
-    
-    while option != 7:
-        Menu.show_menu()
-        option = Menu.request_menu_option()
-        
-        match option:
-            case 1:
-                #Agregar estudiante mientras el usuario así lo desee
-                while True:
-                    add_student(students_list)
-                    if ask_continue_adding() == False:
-                        break;
-                    
-            case 2:
-                #Mostrar estudiantes si la lista no está vacía
-                if len(students_list) == 0:
-                    print("No hay estudiantes agregados.")
-                    input("\nPresione Enter para continuar...")
-                else:
-                    show_Students(students_list)
-                    input("\nPresione Enter para continuar...")
-            case 3:
-                #Mostrar los 3 primeros promedios, si la lista no está vacía
-                if len(students_list) == 0:
-                    print("No hay estudiantes agregados.")
-                    input("\nPresione Enter para continuar...")
-                else:
-                    top3_grades(students_list)
-                    input("\nPresione Enter para continuar...")
-                
-            case 4:
-                #Mostrar todos promedios, si la lista no está vacía
-                if len(students_list) == 0:
-                    print("No hay estudiantes agregados.")
-                    input("Presione Enter para continuar...")
-                else:
-                    calculate_total_averages(students_list)
-                    input("\nPresione Enter para continuar...")
-                
-            case 5:
-                #Si hay datos para guardar, se guarda sobreescribiendo si no existe un archivo o está vacío, 
-                # de lo contrario se pregunta de que modo se quiere guardar la información
-                if len(students_list) == 0:
-                    print("No hay nada por guardar aún.")
-                    input("\nPresione Enter para continuar...")
-                else:
-                    if Data.ask_file_is_new_or_empty('students.csv'):
-                        students_headers = students_list[0].keys()
-                        Data.save_students_overwrite('students.csv', students_list, students_headers)
-                        input("\nPresione Enter para continuar...")
-                    else:
-                        ask_save_data(students_list)
-                        input("\nPresione Enter para continuar...")
-
-            case 6:
-                #Si el archivo CSV no está vacío o existe, se cargan los estudiantes leyendo desde el CSV
-                if Data.ask_file_is_new_or_empty('students.csv'):
-                    print("No hay archivos ni datos que cargar\n")
-                    input("\nPresione Enter para continuar...")
-                else:
-                    students_list = Data.load_students('students.csv')
-                    print(f"Se cargaron {len(students_list)} estudiantes.")
-                    input("\nPresione Enter para continuar...")
-            
-            case 7: 
-                #Salir del programa
-                print("Saliendo...")
-
-
 def input_string(message):
     """
     Se encarga de pedir y validar un string al usuario.
@@ -120,7 +37,8 @@ def input_float(message):
             
             if new_float < 0:
                 print("El campo no puede ser menor a cero.")
-                
+            elif new_float > 100:
+                print("El campo no puede ser mayor a 100.")    
             else:
                 return new_float
             
@@ -154,7 +72,7 @@ def add_student(students_list):
                   'social': social_grade, 
                   'science' : science_grade}
     
-    if not all([student_name, last_student_name, student_group, spanish_grade, english_grade, social_grade, science_grade]):
+    if not all([student_name, last_student_name, student_group]):
         print("Hay campos en blanco. No se agregó el estudiante.")
         return
     
--- Python Básico/Ejercicios Sistema de control/Main.py
+++ Python Básico/Ejercicios Sistema de control/Main.py
@@ -1,9 +1,94 @@
 import Actions
+import Menu
+import Data
+
+def start_menu( ):
+    """
+    Inicia el programa, llama a la funcion que muestra el menú y a la que pide opción al usuario,
+    guía la ejecución del programa, a través de sus 7 opciones principales.
+    Función diseñada para ser llamada desde el main.
+
+    Parámetros:
+        No recibe parámetros.
+
+    Retorna:
+        La función no retorna nada.
+    """
+    students_list = []
+    option = 0
+    
+    while option != 7:
+        Menu.show_menu()
+        option = Menu.request_menu_option()
+        
+        match option:
+            case 1:
+                #Agregar estudiante mientras el usuario así lo desee
+                while True:
+                    Actions.add_student(students_list)
+                    if Actions.ask_continue_adding() == False:
+                        break;
+                    
+            case 2:
+                #Mostrar estudiantes si la lista no está vacía
+                if len(students_list) == 0:
+                    print("No hay estudiantes agregados.")
+                    input("\nPresione Enter para continuar...")
+                else:
+                    Actions.show_Students(students_list)
+                    input("\nPresione Enter para continuar...")
+            case 3:
+                #Mostrar los 3 primeros promedios, si la lista no está vacía
+                if len(students_list) == 0:
+                    print("No hay estudiantes agregados.")
+                    input("\nPresione Enter para continuar...")
+                else:
+                    Actions.top3_grades(students_list)
+                    input("\nPresione Enter para continuar...")
+                
+            case 4:
+                #Mostrar todos promedios, si la lista no está vacía
+                if len(students_list) == 0:
+                    print("No hay estudiantes agregados.")
+                    input("Presione Enter para continuar...")
+                else:
+                    Actions.calculate_total_averages(students_list)
+                    input("\nPresione Enter para continuar...")
+                
+            case 5:
+                #Si hay datos para guardar, se guarda sobreescribiendo si no existe un archivo o está vacío, 
+                # de lo contrario se pregunta de que modo se quiere guardar la información
+                if len(students_list) == 0:
+                    print("No hay nada por guardar aún.")
+                    input("\nPresione Enter para continuar...")
+                else:
+                    if Data.ask_file_is_new_or_empty('students.csv'):
+                        students_headers = students_list[0].keys()
+                        Data.save_students_overwrite('students.csv', students_list, students_headers)
+                        input("\nPresione Enter para continuar...")
+                    else:
+                        Actions.ask_save_data(students_list)
+                        input("\nPresione Enter para continuar...")
+
+            case 6:
+                #Si el archivo CSV no está vacío o existe, se cargan los estudiantes leyendo desde el CSV
+                if Data.ask_file_is_new_or_empty('students.csv'):
+                    print("No hay archivos ni datos que cargar\n")
+                    input("\nPresione Enter para continuar...")
+                else:
+                    students_list = Data.load_students('students.csv')
+                    print(f"Se cargaron {len(students_list)} estudiantes.")
+                    input("\nPresione Enter para continuar...")
+            
+            case 7: 
+                #Salir del programa
+                print("Saliendo...")
+
 
 def Main():
     """
     Función inicial main.
     """
-    Actions.start_menu()
+    start_menu()
 
 Main()
